d4524b77d16304853037376130cd72c9a594e6a9
Elvis Estevan <elvis.estevan65@gmail.com>
2020-02-16 19:21:56 -0300

    #11 - Property binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   title = "Title works!";
+  myString = "i love binding"
 
   constructor() { }
 
