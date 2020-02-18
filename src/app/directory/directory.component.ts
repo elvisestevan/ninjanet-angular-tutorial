7e3db238ef8e4b4ae84df39ac7a3013c86e0a3af
Elvis Estevan <elvis.estevan65@gmail.com>
2020-02-18 17:37:02 -0300

    #19 Directives

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -10,6 +10,8 @@ export class DirectoryComponent implements OnInit {
 
   name: string;
 
+  classes = {"blue": true, "red": false, "underline": true}
+
   constructor(private route: ActivatedRoute) {
     this.name = route.snapshot.params['name'];
   }
