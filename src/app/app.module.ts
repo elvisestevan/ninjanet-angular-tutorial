3ef860b05eacab902eadafbb10677592bf986575
Elvis Estevan <elvis.estevan65@gmail.com>
2020-02-19 17:55:51 -0300

    #24 HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -17,8 +17,9 @@ import { FilterPipe } from './filter.pipe';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,24 @@ import { LoggingService } from '../logging.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private logger: LoggingService) { }
+  quotes: any;
+
+  constructor(private logger: LoggingService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.logger.log(HomeComponent.name);
+
+    this.fetchData();
+
+  }
+
+  fetchData() {
+    this.http.get("https://breaking-bad-quotes.herokuapp.com/v1/quotes/10").subscribe(
+      (res) => {
+        console.log(res);
+        this.quotes = res;
+      }
+    );
   }
 
 }
